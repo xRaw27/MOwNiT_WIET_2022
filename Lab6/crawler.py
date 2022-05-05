a5d91c1e20f77ffcd92be57eb5eff2c78b959ff0
Dariusz Piwowarski <d.piwowarski@onet.pl>
2022-05-05 20:07:59 +0200

    web crawler

---
+++ Lab6/crawler.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://en.wikipedia.org/"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+}
+res = requests.get(url, headers=headers)
+
+# r = requests.get("https://en.wikipedia.org/")
